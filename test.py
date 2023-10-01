c5c7c889229592757eb6bc30eee2946182c04af7
oranshiner <send2oran@gmail.com>
2023-10-01 12:34:37 +0300

    added test

--- test.py
+++ test.py
@@ -19,3 +19,17 @@ def setup_method(self, method):
     self.name = "John Doe"
     self.balance = 1000
     self.isClient = True
+
+    def test_initializes_with_name_balance_and_isClient_flag(self):
+        # Given
+        name = "John Doe"
+        balance = 1000
+        isClient = True
+
+        # When
+        account = BankAccount(name, balance, isClient)
+
+        # Then
+        assert account.name == name
+        assert account.balance == balance
+        assert account.isClient == isClient
