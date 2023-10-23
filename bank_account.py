f7aa5d13458386bbd76f60b031aba7c1ec943f74
oranshiner <send2oran@gmail.com>
2023-10-23 15:19:33 +0300

    Refactor BankAccount class: add validation methods, update deposit and withdraw methods, and rename isClient to is_client

--- bank_account.py
+++ bank_account.py
@@ -1,27 +1,42 @@
 from bank_account_commission import commission
 
-
 class BankAccount:
     """ create a bank account """
-    def __init__(self, name, balance, isClient):
+    def __init__(self, name, balance, is_client):
         self.name = name
         self.balance = balance
-        self.isClient = isClient
+        self.is_client = is_client
 
-    def client(self):
-        """ client information """
+    def get_client_info(self):
+        """ retrieve client information """
         return f'Account name: {self.name}, balance: {self.balance}'
+
+    def validate_amount(self, amount):
+        """ validate the amount """
+        if amount <= 0:
+            raise ValueError("Invalid amount")
 
     def deposit(self, amount):
         """ deposit money """
-        if amount > 0:
-            self.balance += (amount - commission(self.isClient))
-        else:
-            print("Invalid amount")
+        self.validate_amount(amount)
+        self.balance += (amount - commission(self.is_client))
+        return self.balance
 
     def withdraw(self, amount):
         """ withdraw money """
-        if self.balance >= amount > 0:
-            self.balance -= (amount + commission(self.isClient))
+        self.validate_amount(amount)
+        if self.balance >= amount + commission(self.is_client):
+            self.balance -= (amount + commission(self.is_client))
+            return self.balance
+
+    def validate_amount(self, amount):
+        """ validate the amount """
+        if not isinstance(amount, (int, float)):
+            raise ValueError("Invalid amount")
         else:
-            print("Insufficient funds")
+            raise InsufficientFundsError("Insufficient funds")
+
+    def client(self):
+        """ client information """
+        return {'Account name': self.name, 'balance': self.balance}
+
