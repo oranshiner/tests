66c16d6078824679815fb6c9e5fee9863faf22ab
oranshiner <send2oran@gmail.com>
2023-10-23 16:30:29 +0300

    Refactor commission calculation function with improved documentation

--- bank_account_commission.py
+++ bank_account_commission.py
@@ -1,6 +1,13 @@
-def commission(self, isClient):
-    """ charge commision """
+def calculate_commission(isClient: bool) -> float:
+    """ Calculate the commission for a client.
+
+    Parameters:
+    - isClient (bool): Indicates whether the customer is a client or not.
+
+    Returns:
+    - float: The commission amount.
+    """
     if isClient:
         return 2.5
     else:
-        return 2.5
+        return 5
